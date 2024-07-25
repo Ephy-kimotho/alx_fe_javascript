c1e9b5a8f78136b159b5db69708a0d5c9666957d
Ephy Kimotho <ephyk48@gmail.com>
2024-07-25 17:43:30 +0300

    update script js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -12,61 +12,62 @@ addQouteBtn.addEventListener("click", createAddQuoteForm)
 
 const quotes = [
     {
-        quote: "The best way to predict the future is to invent it.",
-        category: "Inspiration"
+      text: "The best way to predict the future is to invent it.",
+      category: "Inspiration"
     },
     {
-        quote: "Life is 10% what happens to us and 90% how we react to it.",
-        category: "Motivation"
+      text: "Life is 10% what happens to us and 90% how we react to it.",
+      category: "Motivation"
     },
     {
-        quote: "Success usually comes to those who are too busy to be looking for it.",
-        category: "Success"
+      text: "Success usually comes to those who are too busy to be looking for it.",
+      category: "Success"
     },
     {
-        quote: "Do not watch the clock. Do what it does. Keep going.",
-        category: "Perseverance"
+      text: "Do not watch the clock. Do what it does. Keep going.",
+      category: "Perseverance"
     },
     {
-        quote: "Your time is limited, don't waste it living someone else's life.",
-        category: "Life"
+      text: "Your time is limited, don't waste it living someone else's life.",
+      category: "Life"
     },
     {
-        quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
-        category: "Individuality"
+      text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+      category: "Individuality"
     },
     {
-        quote: "Happiness is not something ready-made. It comes from your own actions.",
-        category: "Happiness"
+      text: "Happiness is not something ready-made. It comes from your own actions.",
+      category: "Happiness"
     },
     {
-        quote: "The only limit to our realization of tomorrow is our doubts of today.",
-        category: "Potential"
+      text: "The only limit to our realization of tomorrow is our doubts of today.",
+      category: "Potential"
     },
     {
-        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-        category: "Resilience"
+      text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+      category: "Resilience"
     },
     {
-        quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
-        category: "Friendship"
+      text: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+      category: "Friendship"
     }
-];
+  ];
+  
 
 
 function showRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length)
-    const randomQoute = quotes[randomIndex].quote
-    qouteDiv.textContent = randomQoute
+    const randomQoute = quotes[randomIndex].text
+    qouteDiv.innerHTML = `<p>${randomQoute}</p>`
 }
 
 
 function createAddQuoteForm() {
-    const qoute = newQouteText.value.trim()
+    const text = newQouteText.value.trim()
     const category = newQouteCategory.value.trim()
-    if (qoute === "" || category === "") {
+    if (text === "" || category === "") {
         alert("Kindly enter a qoute and it's category")
     } else {
-        quotes.push({ qoute, category })
+        quotes.push({ text, category })
     }
 }
